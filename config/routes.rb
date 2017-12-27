ab235dd93c08ba17f4332c61d2df976e7c503d43
francomuoio <franco.59171@gmail.com>
2017-12-27 11:28:21 +0100

    commit avant pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 Rails.application.routes.draw do
-
-<<<<<<< HEAD
-  get 'pages/acceuil'
-
-  get 'pages/entreprise'
-
-  get 'pages/technologie'
-
-  get 'pages/intervention_sur_site'
-
-  get 'pages/surveillance'
-
-  get 'pages/pièces_de_rechange'
-
-  get 'pages/formation'
-
-  get 'pages/contact'
-
-=======
   get 'accueil', to: 'pages#accueil'
   get 'entreprise', to: 'pages#entreprise'
   get 'technologie', to: 'pages#technologie'
@@ -28,6 +9,5 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   root to: 'pages#accueil'
->>>>>>> c1642ba709b7ea08f838d344d5fae412657f70a4
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
